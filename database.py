3d939c13e3821e3c98bcf487457f26ee7084a32f
atanastenev <atenev@princeton.edu>
2022-03-21 20:50:38 -0400

    added style for tutorial page

--- database.py
+++ database.py
@@ -50,3 +50,36 @@ def searchcards():
     except Exception as ex:
         print(str(sys.argv[0]) + ": " + ex, file=stderr)
         exit(1)
+
+
+
+def addcard():
+    '''Inserts a new credit card into the database'''
+    print (searchquery.__doc__)
+    try:
+        with connect(DATABASE_URL, uri=True) as connection:
+            cursor = connection.cursor()
+            with closing(connection.cursor()) as cursor:
+
+                # the string used to call the SQL commands
+                # to extract specific elements from the database
+                stmt_str="INSERT INTO  "
+                stmt_str+="bonus, pros, cons, details, apply "
+                stmt_str+="FROM creditcards "
+                # param = []
+
+                stmt_str+="ORDER BY dept ASC, coursenum ASC, "
+                stmt_str+="classid ASC "
+
+                cursor.execute(stmt_str)
+                line = cursor.fetchone()
+                cards = []
+                while line is not None:
+                    card = CreditCard(line)
+                    cards.append(card)
+                    line = cursor.fetchone()
+                return cards
+
+    except Exception as ex:
+        print(str(sys.argv[0]) + ": " + ex, file=stderr)
+        exit(1)
