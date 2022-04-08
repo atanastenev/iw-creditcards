816c3dfdbeb6a98d2e71914677db98901663a9dd
atanastenev <atenev@princeton.edu>
2022-04-08 12:18:22 -0400

    changed ways to extract input elements from html

--- credit.py
+++ credit.py
@@ -7,7 +7,7 @@
 
 from sys import stderr
 from creditcard import CreditCard
-from flask import Flask, redirect, session
+from flask import Flask, redirect, request
 from flask import render_template, make_response
 from database import searchcards, addcard
 
@@ -78,17 +78,17 @@ def add_credit_card():
 
     adminpass = "AdminCreditPost"
 
-    name = session.get('name')
-    bank = session.get('bank')
-    annualfee = session.get('aunf')
-    rcs = session.get('rcs')
-    bonus = session.get('bonus')
-    pros = session.get('pros')
-    cons = session.get('cons')
-    details = session.get('details')
-    link = session.get('apply')
-    advice = session.get('advice')
-    key = session.get('key')
+    name = request.form['name']
+    bank = request.form['bank']
+    annualfee = request.form['aunf']
+    rcs = request.form['rcs']
+    bonus = request.form['bonus']
+    pros =request.form['pros']
+    cons = request.form['cons']
+    details = request.form['details']
+    link = request.form['apply']
+    advice = request.form['advice']
+    key = request.form['key']
     print(key)
 
     if key is adminpass:
@@ -103,5 +103,7 @@ def add_credit_card():
             html = render_template('error.html')
             response = make_response(html)
             return response
+
+    print("before going back to table")
 
     return make_response(redirect('/creditable'))
