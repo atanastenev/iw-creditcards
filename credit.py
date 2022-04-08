a90ef9a1e81d221e2201cdf4544c3b5b8fb329e4
atanastenev <atenev@princeton.edu>
2022-04-08 10:37:39 -0400

    added backend to add a credit card

--- credit.py
+++ credit.py
@@ -7,8 +7,8 @@
 
 from sys import stderr
 from creditcard import CreditCard
-from flask import Flask, request, make_response
-from flask import render_template
+from flask import Flask, redirect, session
+from flask import render_template, make_response
 from database import searchcards, addcard
 
 #-----------------------------------------------------------------------
@@ -32,20 +32,10 @@ def index():
 def credit_card_table():
     '''Runs databse of credit cards page.'''
 
-    # name = session.get('name')
-    # bank = session.get('bank')
-    # annualfee = session.get('aunf')
-    # rcs = session.get('rcs')
-    # bonus = session.get('bonus')
-    # pros = session.get('pros')
-    # cons = session.get('cons')
-    # details = session.get('details')
-    # link = session.get('apply')
-    # advice = session.get('advice')
-
-    # if name != "" and bank != "" and annualfee != "" and rcs != "" and\
-    #      bonus != "" and pros != "" and cons != "" and details != ""\
-    #          and link != "" and advice != "":
+    # if name is not None and bank is not None  and annualfee is not None \
+    #     and rcs is not None and bonus is not None and pros is not None \
+    #         and cons is not None and details is not None \
+    #          and link is not None  and advice is not None:
     #     list = [name, bank, annualfee, rcs, bonus, pros, cons]
     #     list.append(details)
     #     list.append(link)
@@ -65,3 +55,33 @@ def credit_card_table():
     html = render_template('creditable.html', cards=cards, title = "Beginner Friendly Credit Cards")
     response = make_response(html)
     return response
+
+
+@app.route('/creditable', methods=['GET', 'POST'])
+def credit_card_table():
+
+    adminpass = "AdminCreditPost"
+
+    name = session.get('name')
+    bank = session.get('bank')
+    annualfee = session.get('aunf')
+    rcs = session.get('rcs')
+    bonus = session.get('bonus')
+    pros = session.get('pros')
+    cons = session.get('cons')
+    details = session.get('details')
+    link = session.get('apply')
+    advice = session.get('advice')
+    key = session.get('key')
+
+    if key is adminpass:
+        line = [name, bank, annualfee, rcs, bonus, pros, cons, details, link, advice]
+        try:
+            card = CreditCard(line)
+        except Exception as excpetion:
+            print(excpetion, file=stderr)
+            html = render_template('error.html')
+            response = make_response(html)
+            return response
+
+    return make_response(redirect('/creditable'))
